f9858cd8cddf325ed603ae8ad1d8a3c5d77d3589
Abhiram Metuku <abhimetuku@gmail.com>
2026-04-22 15:40:29 -0400

    .

--- train.py
+++ train.py
@@ -3,6 +3,8 @@ import os
 import pandas as pd
 import numpy as np
 from datetime import datetime
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
 from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_extraction.text import TfidfVectorizer
 import torch
@@ -11,3 +13,31 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from torch.optim import AdamW
 from transformers import get_linear_schedule_with_warmup
 from clearml import Task
+
+#all configurations
+THRESHOLD = 0.80
+MAX_LEN = 256
+BATCH = 16
+EPOCHS = 3
+LR = 2e-5
+DATA_PATH = "bias_clean.csv"
+VERSION_FILE = "model_version.txt"
+
+import pandas as pd
+df = pd.read_csv("bias_clean.csv")
+print(df["bias"].unique())
+print(df.columns.tolist())
+
+#changing bias labels 
+df["original_bias"] = df["bias"]
+
+# having 3 classes for training so clean
+df["bias"] = df["bias"].replace({
+    "leaning-left": "left",
+    "leaning-right": "right"
+})
+
+# dropping bad rows
+df = df.dropna(subset=["page_text", "bias"])
+
+print(df["bias"].value_counts())
